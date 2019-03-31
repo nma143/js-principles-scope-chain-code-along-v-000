30cf67b72b6504857d691500409c9820bddf3027
Nikki A <nikki.apostolakis@gmail.com>
2019-03-31 20:31:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 /* Your code goes here */
+var customerName = 'bob';
+const leastFavoriteCustomer = "jack"
+
+function upperCaseCustomerName()
+{
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer()
+{
+  bestCustomer = 'not bob'
+}
+function overWriteBestCustomer()
+{
+  bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer()
+{
+  leastFavoriteCustomer = 'joe'
+}
+function attemptTwoFavoriteCustomers()
+{
+  const favoriteCustomer = 'nik'
+  const favoriteCustomer = 'jen'
+}
